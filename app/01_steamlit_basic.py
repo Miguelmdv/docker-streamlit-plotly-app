056150f92078dc273c56941edcf123bb58d8ad6a
Miguel Angel Madrid <m.angelmadrid@gmail.com>
2023-07-01 17:05:28 +0000

    Basic app

---
+++ app/01_steamlit_basic.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df_countries = pd.read_excel("../data/gampinder.xlsx")
+
+import plotly.express as px
+
+mask_year = df_countries.year == 2007
+df_year = df_countries[mask_year]
+
+fig = px.scatter(data_frame=df_year, x="gdpPercap", y="lifeExp",
+           color="continent", size="pop", hover_name="country")
+
+import streamlit as st
+
+st.plotly_chart(fig)
