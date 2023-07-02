be7b3c177394d99462f8c09f0622f7a73c12c7f3
Miguel Angel Madrid <m.angelmadrid@gmail.com>
2023-07-02 10:01:22 +0000

    Sidebar&Animation

---
+++ app/02_streamlit_sidebar.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df_countries = pd.read_excel("../data/gampinder.xlsx")
+
+import plotly.express as px
+
+import streamlit as st
+
+with st.sidebar:
+    year = st.selectbox(label="Select year", 
+                 options=df_countries.year.unique())
+
+mask_year = df_countries.year == year
+df_year = df_countries[mask_year]
+
+fig = px.scatter(data_frame=df_year, x="gdpPercap", y="lifeExp",
+           color="continent", size="pop", hover_name="country")
+
+
+
+st.plotly_chart(fig)
